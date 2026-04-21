14ca989613ca49887243cb91c1dda8729a734ad5
aalokpoonia <aalokpoonia.work@gmail.com>
2026-04-22 02:34:26 +0530

    fix: firebase config with real credentials

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -3,15 +3,15 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "YOUR_KEY",
-  authDomain: "YOUR_DOMAIN",
-  projectId: "YOUR_ID",
-  storageBucket: "YOUR_BUCKET",
-  messagingSenderId: "XXXX",
-  appId: "XXXX"
+  apiKey: "AIzaSyCXxUIJ_r-QwbcLs7ty0eXoWpydvN324ZY",
+  authDomain: "swasthsetu-admin.firebaseapp.com",
+  projectId: "swasthsetu-admin",
+  storageBucket: "swasthsetu-admin.firebasestorage.app",
+  messagingSenderId: "769856858254",
+  appId: "1:769856858254:web:e2df6e7d23a8b98a1401df",
+  measurementId: "G-WPXKV7Q11P"
 };
 
 const app = initializeApp(firebaseConfig);
-
 export const auth = getAuth(app);
 export const db = getFirestore(app);

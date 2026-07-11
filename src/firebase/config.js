60f72bc9ab03fd25d038c6f19218877c5123e066
aalokpoonia <aalokpoonia.work@gmail.com>
2026-07-11 18:30:23 +0530

    fix: correct firebase project config

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -3,13 +3,13 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCXxUIJ_r-QwbcLs7ty0eXoWpydvN324ZY",
-  authDomain: "swasthsetu-admin.firebaseapp.com",
-  projectId: "swasthsetu-admin",
-  storageBucket: "swasthsetu-admin.firebasestorage.app",
-  messagingSenderId: "769856858254",
-  appId: "1:769856858254:web:e2df6e7d23a8b98a1401df",
-  measurementId: "G-WPXKV7Q11P"
+  apiKey: "AIzaSyArlEPCb0J42gwRtVgfD44S4atOV43Z4js",
+  authDomain: "swasthsetu-admin-d8802.firebaseapp.com",
+  projectId: "swasthsetu-admin-d8802",
+  storageBucket: "swasthsetu-admin-d8802.firebasestorage.app",
+  messagingSenderId: "312019653035",
+  appId: "1:312019653035:web:f117a03cfd92dc5845c2ee",
+  measurementId: "G-Y1261LVQVD"
 };
 
 const app = initializeApp(firebaseConfig);
